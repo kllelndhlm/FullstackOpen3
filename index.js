6ead4c79b96a52af70999c291c14553c6a6f275e
Kalle Lindholm <kalle.lindholm@helsinki.fi>
2024-02-07 13:42:28 +0200

    Part 3 Exercise 3

--- index.js
+++ index.js
@@ -37,6 +37,19 @@ app.get('/api/persons', (req, res) => {
     res.json(persons)
   })
 
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const person = persons.find(person => person.id === id)
+
+  if (person) {
+    res.json(person)
+  } else {
+    res.status(404).end()
+  }
+
+})
+  
+
 app.get('/info', (req, res) => {
     res.send('<p>Phonebook has info on '+persons.length+' people</p><p>'+dateTimeObject.toDateString()+' '+dateTimeObject.toTimeString()+'</p>')
     })
