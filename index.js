e1eae1ba4bcd1fd219581597d0f5da9447b021de
Kalle Lindholm <kalle.lindholm@helsinki.fi>
2024-02-07 13:50:10 +0200

    Part 3 Exercise 4

--- index.js
+++ index.js
@@ -46,9 +46,14 @@ app.get('/api/persons/:id', (req, res) => {
   } else {
     res.status(404).end()
   }
-
 })
   
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  persons = persons.filter(person => person.id !== id)
+
+  res.status(204).end()
+})
 
 app.get('/info', (req, res) => {
     res.send('<p>Phonebook has info on '+persons.length+' people</p><p>'+dateTimeObject.toDateString()+' '+dateTimeObject.toTimeString()+'</p>')
