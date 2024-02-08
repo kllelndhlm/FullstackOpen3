7ac100034e0d60133a6abb3491bdcec2cc9beb38
Kalle Lindholm <kalle.lindholm@helsinki.fi>
2024-02-08 11:47:03 +0200

    Install cors

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const morgan = require('morgan');
 const app = express()
+const cors = require('cors')
+
+app.use(cors())
 
 app.use(express.json())
 
@@ -108,6 +111,7 @@ app.post('/api/persons', (req, res) => {
   res.json(persons)
 })
 
-const PORT = 3001   
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
